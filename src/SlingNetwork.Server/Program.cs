ec47594eda674ff2424346417d7c1e32b17749f6
walterlv <xblero@gmail.com>
2026-07-07 19:06:44 +0800

    加入空项目文件

---
+++ src/SlingNetwork.Server/Program.cs
@@ -0,0 +1,8 @@
+namespace SlingNetwork.Server;
+
+internal static class Program
+{
+    private static void Main()
+    {
+    }
+}
---
+++ src/SlingNetwork/Program.cs
@@ -0,0 +1,8 @@
+namespace SlingNetwork;
+
+internal static class Program
+{
+    private static void Main()
+    {
+    }
+}
