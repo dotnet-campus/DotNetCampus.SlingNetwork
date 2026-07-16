66e8c3146546d69587ef8897fd3b9b34560f8eca
walterlv <xblero@gmail.com>
2026-07-16 09:26:47 +0800

    获取反代后的 IP

--- src/SlingNetwork/ServerSide/ControlServices/IPWebApi.cs
+++ src/SlingNetwork/ServerSide/ControlServices/IPWebApi.cs
@@ -1,4 +1,5 @@
-﻿using TouchSocket.Http;
+﻿using System.Net;
+using TouchSocket.Http;
 using TouchSocket.Rpc;
 using TouchSocket.WebApi;
 
@@ -10,6 +11,17 @@ public class IPWebApi : SingletonRpcServer
     [WebApi(Method = HttpMethodType.Get)]
     public string IP(IWebApiCallContext context)
     {
+        // 当被 Nginx 代理后，需要通过 HTTP 头获取真实客户端 IP
+        var rawIP = context.HttpContext.Request.Headers
+            .Get("X-Real-IP")
+            .First;
+
+        if (IPAddress.TryParse(rawIP, out var forwardedIP))
+        {
+            return forwardedIP.ToString();
+        }
+
+        // 其他情况，视请求 IP 为客户端 IP
         if (context.Caller is IHttpSessionClient httpSessionClient)
         {
             return httpSessionClient.IP;
